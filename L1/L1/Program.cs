4aca01f81392be1b2038c8a56af49e3a1c222f1d
Fabian Bucher <EFABBUC@ADS.KTAG.CH>
2016-03-22 15:56:11 +0100

    Money-Klasse erstellt mit Operator-Overload

---
+++ L1/L1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new Money(10, Currency.Franken);
+            Money m2 = new Money(20, Currency.Franken);
+            Console.Write((m1 + m2).Amount);
+            Console.Write(" " + (m1 + m2).Currency.ToString());
+            Console.ReadKey();
+        }
+    }
+
+    public class Money
+    {
+        public double Amount { get; set; }
+
+        public Currency Currency { get; set; }
+
+        public Money(double amount, Currency currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            return new Money(m1.Amount + m2.Amount, m1.Currency);
+        }
+
+    }
+
+    public enum Currency
+    {
+        Franken,
+        Euro,
+        Dollar
+    }
+}
