d2cd3fe1f9777de6392ec04cbb758f5d815733fa
Fabian Bucher <EFABBUC@ADS.KTAG.CH>
2016-03-23 08:40:56 +0100

    Initialize MoneyUnitTests

---
+++ L1/L1.Test/MoneyUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L1.Test
+{
+    [TestClass]
+    public class MoneyUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
